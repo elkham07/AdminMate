fff72f5950371a0cde22a5b41d00aa51c642a201
elkham07 <elkhammamedov@gmail.com>
2026-03-20 22:35:42 +0500

    add database just simple JSON files

--- adminmate/bot.py
+++ adminmate/bot.py
@@ -16,3 +16,13 @@ intents.presences = True
  
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+def load_data(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
+    return {}
+ 
+def save_data(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2)
+
