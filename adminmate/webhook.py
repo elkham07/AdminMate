a281c3188d2908e1ff6bb68573e0befda7610771
elkham07 <elkhammamedov@gmail.com>
2026-03-21 03:52:46 +0500

    add port config

--- adminmate/webhook.py
+++ adminmate/webhook.py
@@ -44,4 +44,5 @@ async def handle_role(event, discord_id):
         await member.remove_roles(role)
 
 def run_flask():
-    app.run(host='0.0.0.0', port=5005)
+    port = int(os.environ.get('PORT', 5005))
+    app.run(host='0.0.0.0', port=port)
